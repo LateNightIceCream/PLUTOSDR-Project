27a3d471b98319b924baa4c70edbc4a41dac2b96
LateNightIceCream <st181028@hs-wismar.de>
2023-12-06 15:47:52 +0100

    add a note

--- src/util/frequency_offset/tx_main.m
+++ src/util/frequency_offset/tx_main.m
@@ -5,7 +5,14 @@ SP = PR_QAM_Tx_Init
 
 % send the signal for frequency offset calibration
 disp("Transmitting offset calibration signals...")
-[SP, tx] = frequency_offset_transmit(SP, 80e3);
+[SP, tx] = frequency_offset_transmit(SP, 80e3); % we cannot release the tx structure because it changes the frequency offset of the hardware for some reason
+
+% Problem: setting up Tx with some number of samples per frame
+% then => inside runPRQAM... we call the bits generator which might add
+% some padding => could be different sizes
+% and we cant change the tx samplesperframe without releasing it (and
+% creating a new one)
+% so it needs to be correct from the beginning
 
 % send the message
 disp("Transmitting message...")
