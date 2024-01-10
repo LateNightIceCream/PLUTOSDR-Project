75496f656a6dfcbbefad4850b1055d6bd5eabd37
LateNightIceCream <richardgruenert@live.com>
2024-01-10 14:02:42 +0100

    update class rewrite

--- src/util/common/transmitter/Transmitter.m
+++ src/util/common/transmitter/Transmitter.m
@@ -1,52 +1,20 @@
-classdef Transmitter
-    %% Singleton transmitter object for Pluto
+classdef Transmitter < RadioBase
+    %% Transmitter object for adalm Pluto
     
     properties (Constant)
-        
-        BITS_PER_CHARACTER = 16; % do not change this
-        
-        %% General parameters
-        Rsym = 0.2e6;             % Symbol rate in Hertz
-        ModulationOrder = 4;      % QPSK alphabet size
-        Interpolation = 7;        % Interpolation factor
-        Decimation = 1;           % Decimation factor
-        
-        %% Frame Specifications
-        BarkerCode = [+1 +1 +1 +1 +1 -1 -1 +1 +1 -1 +1 -1 +1]; % Bipolar Barker Code
         NumberOfMessages = 100; % Number of messages in a frame
-        SizeFieldLength = 16;
-        
-        %% Tx Parameters
-        RolloffFactor = 0.5; % Rolloff Factor of Raised Cosine Filter
-        ScramblerBase = 2;
-        ScramblerPolynomial = [1 1 1 0 1];
-        ScramblerInitialConditions = [0 0 0 0];
-        RaisedCosineFilterSpan = 10; % Filter span of Raised Cosine Tx Rx filters (in symbols)
-
     end
     
     properties (GetAccess=public, SetAccess=private)
-        % length in bits of the frame's size field
-        Message = 'Hello Ŵorld'; % character vector, make sure to use SINGLE QUOTES
-        %FrameSize       = (HeaderLength + SizeFieldLength + PayloadLength + numel(calc_padding_bits(HeaderLength + SizeFieldLength + PayloadLength, ModulationOrder))) ...
-        %    / log2(ModulationOrder);                                    % Frame size in symbols
-        %FrameTime       = Tsym*FrameSize;
-        Symbols = [];
+        Message = 'Replace this message by calling init_radio';
     end
     
     properties (Dependent)
-        Tsym;
-        Fs;
-        %% Frame related
-        Header;
         MessageLength;
-        PayloadLength;
+        PayloadSizeBits;
         FrameSizeBits;
         FrameSizeSymbols;
         FrameSizeSamples;
-    end
-    
-    properties (Access=private)
     end
     
     methods
@@ -55,8 +23,168 @@ classdef Transmitter
         end
         
         
-        %% Initialize the Pluto Radio object
-        function init_transmission(obj, message)
+        %% Initialize the Pluto Radio Object
+        function init_radio(obj, pluto_address, message)
+            
+            % the message needs to be known beforehand because
+            % the total frame size for the pluto has to be correct
+            % from the beginning to carry the offset setting over.
+            % TODO: this is only relevant for the receiver, no?
+            
+            obj.init_message_transmission(message);
+            
+            obj.PlutoAddress = pluto_address;
+            
+            if ~isempty(obj.Radio)
+                release(obj.Radio);
+            end
+            
+            % Use the default value of 0 for FrequencyCorrection, which corresponds to
+            % the factory-calibrated condition
+            obj.Radio = sdrtx('Pluto', ...
+                              'RadioID', obj.PlutoAddress, ...
+                              'CenterFrequency', obj.CenterFrequency, ...
+                              'BasebandSampleRate', obj.Fs, ...
+                              'Gain', obj.PlutoGain, ...
+                              'ShowAdvancedProperties', true);
+            info(obj.Radio);
+        end
+        
+        
+        %% Transmit the message symbols via Pluto
+        function transmit_message(obj, duration_sec)
+            
+            if isempty(obj.Radio)
+                error("Radio object not initialized. Call init_radio(...) first.");
+            end
+            
+            if isempty(obj.Symbols)
+                error("No symbols to send.");
+            end
+            
+            %radio.Gain = obj.PlutoGain;
+            time_per_sample = obj.FrameSizeSamples / obj.Fs;
+
+            step(obj.Radio, obj.Symbols);
+            disp('Transmission has started')
+            currentTime = 0;
+            % Transmission Process
+            while currentTime < duration_sec
+                % Data transmission
+                step(obj.Radio, obj.Symbols);
+
+                % Update simulation time
+                currentTime = currentTime + time_per_sample;
+            end
+
+            if currentTime ~= 0
+                disp('Transmission has ended')
+            end
+
+            release(radio);
+        end
+
+        
+        %% Transmit frequency offset signals
+        function transmit_frequency_offset_signals(obj, duration_sec)
+            
+            if isempty(obj.Radio)
+                error("Radio object not initialized. Call init_radio(...) first.");
+            end
+            
+            sampleRate = obj.Fs;
+            numSamples = obj.FrameSizeSamples;
+    
+            function s = get_ref_sin(frequency)
+                sw = dsp.SineWave;
+                sw.Amplitude = 1.0;
+                sw.Frequency = frequency;
+                sw.ComplexOutput = true;
+                sw.SampleRate = sampleRate;
+                sw.SamplesPerFrame = numSamples;
+                s = sw();
+            end
+
+            fRef = obj.FRef;
+            
+            % Send signals
+            disp('Sending 3 tones at 20, 40, and 80 kHz');
+
+            % do not use transmitRepeat because we dont want to release the
+            % transmitter
+            % transmitRepeat(tx, s);
+
+            s = get_ref_sin(20e3) + get_ref_sin(40e3) + get_ref_sin(fRef);
+            s = 0.6*s/max(abs(s));
+
+            RefSigTime = numSamples / sampleRate;
+            
+            % Transmission Process
+            step(obj.Radio, s); % for connecting
+            disp('Transmission has started');
+            fprintf("Running Offset Transmission for %f seconds\n", duration_sec)
+            currentTime = 0;
+            while currentTime < duration_sec
+                step(obj.Radio, s);
+                currentTime = currentTime + RefSigTime;
+            end
+
+            if currentTime ~= 0
+                disp('Transmission has ended')
+            end
+        end
+        
+        %% Setters
+        
+        % Message
+        function set.Message(obj, message)
+            if ~ischar(message)
+                error("Message should be character array. Remember to use single quotes instead of double quotes for literals.")
+            end
+            obj.Message = message;
+        end
+        
+        
+        %% Getters
+        
+        % Message Length (characters)
+        function MessageLength = get.MessageLength(obj)
+            MessageLength = strlength(obj.Message);
+        end
+        
+        
+        % Payload Length (bits)
+        function PayloadSizeBits = get.PayloadSizeBits(obj)
+            PayloadSizeBits = obj.NumberOfMessages * obj.MessageLength * obj.BITS_PER_CHARACTER;
+        end
+        
+        
+        % Total frame size including message (bits)
+        function FrameSizeBits = get.FrameSizeBits(obj)
+            useful_size = length(obj.Header) + obj.SizeFieldLength + obj.PayloadSizeBits;
+            padding_size = numel(calc_padding_bits(useful_size, obj.ModulationOrder));
+            FrameSizeBits = (useful_size + padding_size);
+        end
+        
+        
+        % Total frame size in Symbols
+        function FrameSizeSymbols = get.FrameSizeSymbols(obj)
+            FrameSizeSymbols = obj.FrameSizeBits / log2(obj.ModulationOrder);
+        end
+        
+        
+        % Total frame size in Samples (without sampling rate)
+        function FrameSizeSamples = get.FrameSizeSamples(obj)
+            FrameSizeSamples = obj.FrameSizeSymbols * obj.Interpolation;
+        end
+        
+    end
+    
+    
+    methods (Access=private)
+        
+        %% Prepare the message transmission
+        function obj = init_message_transmission(obj, message)
             obj.Message = message;
             frame_bits = obj.generate_frame_bits();
             obj.Symbols = obj.generate_symbols(frame_bits);
@@ -108,96 +236,8 @@ classdef Transmitter
         end
         
         
-        %% Transmit the symbols via Pluto
-        function send_message(obj)
-            if isempty(obj.Symbols)
-                error("No symbols to send.");
-            end
-            
-        end
-        
-        
-        %% Setters
-        
-        % Message
-        function obj = set.Message(obj, message)
-            if ~ischar(message)
-                error("Message should be character array. Remember to use single quotes instead of double quotes for literals.")
-            end
-            disp("DONE")
-            obj.Message = message;
-        end
-        
-        
-        %% Getters
-        
-        % Symbol duration (s)
-        function Tsym = get.Tsym(obj)
-            Tsym = 1/obj.Rsym;
-        end
-        
-        
-        % Sample rate (Hz)
-        function Fs = get.Fs(obj)
-            Fs = obj.Interpolation * obj.Rsym;
-        end
-        
-        
-        % Header (bipolar array)
-        function Header = get.Header(obj)
-            ubc = ((obj.BarkerCode + 1) / 2)';
-            Header = (repmat(ubc,2,1))';
-        end
-        
-        
-        % Message Length (characters)
-        function MessageLength = get.MessageLength(obj)
-            MessageLength = strlength(obj.Message);
-        end
-        
-        
-        % Payload Length (bits)
-        function PayloadLength = get.PayloadLength(obj)
-            PayloadLength = obj.NumberOfMessages * obj.MessageLength * obj.BITS_PER_CHARACTER;
-        end
-        
-        
-        % Total frame size including message (bits)
-        function FrameSizeBits = get.FrameSizeBits(obj)
-            useful_size = length(obj.Header) + obj.SizeFieldLength + obj.PayloadLength;
-            padding_size = numel(calc_padding_bits(useful_size, obj.ModulationOrder));
-            FrameSizeBits = (useful_size + padding_size);
-        end
-        
-        
-        % Total frame size in Symbols
-        function FrameSizeSymbols = get.FrameSizeSymbols(obj)
-            FrameSizeSymbols = obj.FrameSizeBits * obj.Interpolation / log2(obj.ModulationOrder);
-        end
-        
-        
-        % Total frame size in Samples
-        function FrameSizeSamples = get.FrameSizeSamples(obj)
-            FrameSizeSamples = obj.FrameSizeSymbols * obj.Fs;
-        end
-        
-    end
-    
-    methods (Access=private)
-        
-        % convert number of bits to number of samples
-        % TODO: need to test this
-        function nsamples = num_samples(obj, nbits)
-            nsymbols = obj.num_symbols(nbits);
-            nsamples = nsymbols * obj.Fs * obj.Interpolation;
-        end
-        
-        % convert number of bits to number of symbols
-        function nsymbols = num_symbols(obj, nbits)
-            nsymbols = nbits / log2(obj.ModulationOrder);
-        end
-        
-        % repeat the message and convert the message string to
+        %% Converts message to payload bits
+        % repeats the message and converts the message string to
         % a bit array
         function bits = generate_payload_bits(obj)
             numMessages = obj.NumberOfMessages;
@@ -211,6 +251,7 @@ classdef Transmitter
             bits = repmat(bits, 1, numMessages);
         end
         
+        %%
         % used to test that the payload bits are generated correctly and
         % can be recovered to characters
         function test_payload_generation(obj)
@@ -220,7 +261,7 @@ classdef Transmitter
             disp("bitlength:");
             disp(length(bits));
             disp("calclength:");
-            disp(obj.PayloadLength);
+            disp(obj.PayloadSizeBits);
             obj.BITS_PER_CHARACTER * obj.NumberOfMessages
             % perform the reverse
             bitrows = reshape(bits, obj.BITS_PER_CHARACTER, obj.NumberOfMessages * obj.MessageLength)
@@ -228,6 +269,18 @@ classdef Transmitter
             bitrows'
             nums = bi2de(bitrows', 'left-msb')
             char(nums)
+        end
+
+        % convert number of bits to number of samples
+        % TODO: need to test this
+        function nsamples = num_samples(obj, nbits)
+            nsymbols = obj.num_symbols(nbits);
+            nsamples = nsymbols * obj.Interpolation;
+        end
+        
+        % convert number of bits to number of symbols
+        function nsymbols = num_symbols(obj, nbits)
+            nsymbols = nbits / log2(obj.ModulationOrder);
         end
         
     end
